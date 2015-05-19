f581d96ea3ded25be3544865126abaa48fdc2681
Sean Fooks <seanfooks@gmail.com>
2015-05-18 23:13:31 -0700

    Added table header from donut.js to list hours of operation in th

--- donut.js
+++ donut.js
@@ -1,16 +1,41 @@
+window.onload = function(){
 var StoreLocation = function(name, hours, avgDonutsPerCust, minCust, maxCust){
   this.name = name;
   this.hours = hours;
   this.avgDonutsPerCust = avgDonutsPerCust;
   this.minCust = minCust;
   this.maxCust = maxCust;
-  this.custPerHour = [];
+  this.donutsPerHour = [];
   for(var i = 0; i < this.hours.length; i++){
-    this.custPerHour[i] = this.minCust + Math.floor(Math.random() * (this.maxCust - this.minCust));
+    var custPerHour = this.minCust + Math.floor(Math.random() * (this.maxCust - this.minCust));
+    this.donutsPerHour[i] = custPerHour * this.avgDonutsPerCust;
   }
 }
 
-var hours=["7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"]
+var hours=["7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"];
 
 var downtown = new StoreLocation("Downtown", hours, 3, 10, 25);
 var caphill = new StoreLocation("Capitol Hill", hours, 4, 15, 50);
+var bothell = new StoreLocation("Bothell", hours, 2, 5, 27);
+var auburn = new StoreLocation("Auburn", hours, 3, 8, 15);
+var bellevue = new StoreLocation("Bellevue", hours, 4, 15, 40);
+
+var locations=[downtown, caphill, bothell, auburn, bellevue];
+
+var hoursOperation = document.getElementById("hours_operation");
+for(var i = 0; i < hours.length; i++){
+  var tableHead = document.createElement("th");
+  hoursOperation.appendChild(tableHead);
+  tableHead.textContent = hours[i];
+}
+
+
+}
+
+
+
+
+
+
+
+
